0d5b68bc3cfca9e425c0eeef91bac240f07e1ecd
xiumu <xiumu@dian.so>
2018-06-27 00:13:25 +0800

    save

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const Koa = require('koa2')
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = 'Hello World'
+  console.log(ctx)
+  if (~ctx.request.url.indexOf('test')) {
+    ctx.body = 'Hello test'
+    return
+  }
+  ctx.body = JSON.stringify({success: true, data: {name: '测试'}})
 })
 
 app.listen(3000)
