5b82a17b03ce326af4dd1a6f7abcd0b44b7c991c
xiumu <xiumu@dian.so>
2018-06-27 16:47:20 +0800

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = new Koa()
 app.use(async ctx => {
   console.log(ctx)
   if (~ctx.request.url.indexOf('test')) {
-    ctx.body = 'Hello test'
+    ctx.body = 'Hello test~'
     return
   }
   ctx.body = JSON.stringify({success: true, data: {name: '测试'}})
